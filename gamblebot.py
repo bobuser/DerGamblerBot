7d0b348963acf955a60aec12a1c9127c20bfdc83
Leo <1238605+bobuser@users.noreply.github.com>
2020-11-08 14:09:14 +0100

    added reading token from file and .idea to gitignore

--- gamblebot.py
+++ gamblebot.py
@@ -5,14 +5,11 @@
 # This program is dedicated to the public domain under the CC0 license.
 
 """
-Simple Bot to reply to Telegram messages.
 
 First, a few handler functions are defined. Then, those functions are passed to
 the Dispatcher and registered at their respective places.
 Then, the bot is started and runs until we press Ctrl-C on the command line.
 
-Usage:
-Basic Echobot example, repeats messages.
 Press Ctrl-C on the command line or send a signal to the process to stop the
 bot.
 """
@@ -34,9 +31,9 @@ logger = logging.getLogger(__name__)
 # context. Error handlers also receive the raised TelegramError object in error.
 def start(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /start is issued."""
-    output=""
+    output = ""
     for user in context.chat_data.values():
-      output=output+"{} : {} ".format(user.get("name"),user.get("score"))
+        output = output + "{} : {} ".format(user.get("name"), user.get("score"))
     update.message.reply_text(output)
 
 
@@ -45,29 +42,39 @@ def help_command(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Help!')
 
 
-def echo(update: Update, context: CallbackContext) -> None:
-    """Echo the user message. [22,64,43,1]"""
-    if(update.message.dice.value in [22,64,43,1] and ('\U0001F3B0'==update.message.dice.emoji)): #(update.message.dice.emoji).encode('utf-8')==b'\xf0\x9f\x8e\xb0'):
-      user_id=update.effective_user.id
-      name=update.effective_user.first_name
-      print(context.chat_data)
-      if(not user_id in context.chat_data):
-        context.chat_data[user_id] = {"name": name, "score" : 0}
-        print(context.chat_data)
-        if(user_id==436304467): context.chat_data[user_id].update({"score" : 99})
-      else:
-        context.chat_data[user_id].update({"name": name})
-      if(context.chat_data.get(user_id) and context.chat_data.get(user_id).get("score")):
-        context.chat_data[user_id]["score"] = context.chat_data[user_id]["score"]+1
-        update.message.reply_text("GZ {}. Now you have {} points.".format(update.message.from_user.first_name, context.chat_data[user_id]["score"]))
-      else:
-        if(not context.chat_data):
-          context.chat_data[user_id].update({"score" : 1})
-        else:
-          context.chat_data[user_id].update({"score" : 1})
-        update.message.reply_text("GZ {}. Now you have {} point.".format(update.message.from_user.first_name, context.chat_data[user_id]["score"]))
+def update_score(user_id, user_name, update: Update, context: CallbackContext) -> None:
+    if (not user_id in context.chat_data):
+        context.chat_data[user_id] = {"name": user_name, "score": 0}
+    else:
+        context.chat_data[user_id].update({"name": user_name})
+    if (context.chat_data.get(user_id) and context.chat_data.get(user_id).get("score")):
+        context.chat_data[user_id]["score"] += 1
+        update.message.reply_text(
+            "GZ {}. Now you have {} points.".format(user_name, context.chat_data[user_id]["score"]))
+    else:
+        context.chat_data[user_id].update({"score": 1})
+        update.message.reply_text("GZ {}. Now you have {} point.".format(update.message.from_user.first_name,
+                                                                         context.chat_data[user_id]["score"]))
+    del user_id
+    del user_name
     print(context.chat_data)
-    #del user_id
+
+
+def check_msg(update: Update, context: CallbackContext) -> None:
+    """win values: [22,64,43,1]"""
+    if (update.message.dice.value in range(100) and (
+            '\U0001F3B0' == update.message.dice.emoji)):  # (update.message.dice.emoji).encode('utf-8')==b'\xf0\x9f\x8e\xb0'):
+        user_id = update.effective_user.id
+        user_name = update.effective_user.first_name
+        update_score(user_id, user_name, update, context)
+    del user_id
+    del user_name
+
+
+def read_token():
+    f = open("token.txt", "r")
+    token = str(f.readline())
+    return token
 
 
 def main():
@@ -76,18 +83,20 @@ def main():
     # Create the Updater and pass it your bot's token.
     # Make sure to set use_context=True to use the new context based callbacks
     # Post version 12 this will no longer be necessary
-    updater = Updater("TOKEN", persistence=pp, use_context=True)
+    updater = Updater(read_token(), persistence=pp, use_context=True)
 
     # Get the dispatcher to register handlers
     dispatcher = updater.dispatcher
 
     # on different commands - answer in Telegram
-    dispatcher.add_handler(CommandHandler("leaderboard", start))
-    dispatcher.add_handler(CommandHandler("points", start))
-    dispatcher.add_handler(CommandHandler("help", help_command))
+    dispatcher.add_handler(CommandHandler("leaderboard", start, ~Filters.update.edited_message))
+    dispatcher.add_handler(CommandHandler("points", start, ~Filters.update.edited_message))
+    dispatcher.add_handler(CommandHandler("help", help_command, ~Filters.update.edited_message))
 
-    # on noncommand i.e message - echo the message on Telegram
-    dispatcher.add_handler(MessageHandler(Filters.dice & ~Filters.command & ~Filters.forwarded & ~Filters.dice.darts & ~Filters.dice.basketball & ~Filters.dice.dice, echo))
+    # the actual messages to reply to
+    dispatcher.add_handler(MessageHandler(
+        Filters.dice & ~Filters.command & ~Filters.forwarded & ~Filters.dice.darts & ~Filters.dice.basketball & ~Filters.dice.dice,
+        check_msg))
 
     # Start the Bot
     updater.start_polling()
